6ebed7af0475b315f4fbaf92a3870a220cc2ef84
Aman <amanv9617@gmail.com>
2026-06-29 15:44:27 +0530

    add a file

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -8,7 +8,7 @@ export default function Footer() {
 
       {/* Top Section */}
       <div className="max-w-7xl mx-auto px-6 py-10 grid grid-cols-1 md:grid-cols-5 gap-10">
-
+{/* asdiasd */}
         {/* Left Section */}
         <div className="md:col-span-2">
 
